c22376fc6c8882c8f8339af7acfce6c2665285dd
Aria Buckles <aria@classjourney.org>
2017-08-04 17:46:04 -0700

    radio: not compiling

--- src/ArtemisWidgets.js
+++ src/ArtemisWidgets.js
@@ -5,6 +5,7 @@ export const defaultEditors = () => {
     'image': require('./widgets/ImageEditor').default,
     'inline-math': require('./widgets/InlineMathEditor').default,
     'numeric-input': require('./widgets/NumericInputEditor').default,
+    'radio': require('./widgets/Radio').default,
   };
   return defaultEditorCache;
 };
---
+++ src/widgets/Radio.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+import '../FontAwesome';
+import { View, StyleSheet } from '../base-components';
+
+import Popover from '../helpers/Popover';
+
+import '../lib/perseus/perseus-css';
+import PerseusRadioEditor from '../lib/perseus/widgets/radio/editor';
+import {
+  widget as PerseusRadio,
+  transform as PerseusRadioTransform
+} from '../lib/perseus/widgets/radio';
+
+const styles = StyleSheet.create({
+  radio: {
+    display: 'flex',
+    minWidth: 100,
+    boxSizing: 'border-box',
+    position: 'relative',
+    overflow: 'hidden',
+    borderStyle: 'solid',
+    borderColor: 'rgb(186, 190, 194)',
+    borderWidth: 1,
+    borderRadius: 4,
+    minHeight: 100,
+    padding: '6px 6px 1px',
+    textAlign: 'center',
+    color: 'black',
+  },
+
+  editor: {
+    color: '#444444',
+    width: 360,
+    height: 360,
+    backgroundColor: 'white',
+    border: `2px solid #4FAED1`,
+    borderRadius: 5,
+    margin: 8,
+    padding: 10,
+  },
+});
+
+const EDITOR_PAD_WIDTH = 16;
+
+export default class ImageEditor extends Component {
+
+  render() {
+    return <Popover>
+      <View className="framework-perseus" style={styles.radio}>
+        {/* responsive false because we have no size info in artemis
+            overlays */}
+        <PerseusRadio
+          {...PerseusRadioTransform(this.props)}
+          _clampWidth={this.props._maxWidth - EDITOR_PAD_WIDTH}
+          />
+      </View>
+      <View className="framework-perseus" style={styles.editor}>
+        <PerseusRadioEditor
+          {...this.props}
+          onChange={this.props.onChange}
+        />
+      </View>
+    </Popover>;
+  }
+}
+
